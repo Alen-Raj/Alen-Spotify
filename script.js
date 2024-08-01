55a1dbe30e630387ad859ce19272e4c37dd94e5a
Alen-Raj <alenraj246@gmail.com>
2024-08-02 00:39:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+function toggleMenu(){
+  var sidebar = document.getElementById("nav-list");
+  if (sidebar.style.display === "none")  {
+    sidebar.style.display = "block";
+  }
+  else {
+    sidebar.style.display = "none";
+  }
+}
+
+
+function click1(){
+
+  var flist = document.getElementById("li1");
+  if (flist.style.display === "none") {
+    flist.style.display ="block";
+  }
+  else {
+    flist.style.display = "none";
+  }
+
+}
+function click2(){
+
+  var slist = document.getElementById("li2");
+  if (slist.style.display === "none") {
+    slist.style.display ="block";
+  }
+  else {
+    slist.style.display = "none";
+  }
+
+}
+function click3(){
+
+  var tlist = document.getElementById("li3");
+  if (tlist.style.display === "none") {
+    tlist.style.display ="block";
+  }
+  else {
+    tlist.style.display = "none";
+  }
+
+}
+function click4(){
+
+  var frlist = document.getElementById("li4");
+  if (frlist.style.display === "none") {
+    frlist.style.display ="block";
+  }
+  else {
+    frlist.style.display = "none";
+  }
+
+}
+function click5(){
+
+  var fvlist = document.getElementById("li5");
+  if (fvlist.style.display === "none") {
+    fvlist.style.display ="block";
+  }
+  else {
+    fvlist.style.display = "none";
+  }
+
+}
